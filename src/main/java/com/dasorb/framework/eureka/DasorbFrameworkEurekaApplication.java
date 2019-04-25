c9102c66a91c68474b9904071d1363965cb2422d
chenjie <chenjie@atominds.com>
2019-04-25 10:26:18 +0800

    Initial commit

---
+++ src/main/java/com/dasorb/framework/eureka/DasorbFrameworkEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.dasorb.framework.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasorbFrameworkEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DasorbFrameworkEurekaApplication.class, args);
+    }
+
+}
